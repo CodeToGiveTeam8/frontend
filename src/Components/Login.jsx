2286c5f776559f17e7fb83e20c8ffbeff4242adb
Ananta Nath <nathananta0001@gmail.com>
2023-06-06 04:53:58 +0530

    login

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,9 +1,106 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import './CSS styles/Login.css';
 
-function Login() {
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState('');
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    // You can implement your login logic here
+    // For simplicity, let's just check if the username and password match "admin"
+    if (username === 'admin' && password === 'admin') {
+      // If the login is successful, redirect to the corresponding page based on the selected category
+      switch (selectedCategory) {
+        case 'representative':
+          navigate('/representative-page');
+          break;
+        case 'team-lead':
+          navigate('/team-lead-page');
+          break;
+        case 'operations':
+          navigate('/operations-page');
+          break;
+        default:
+          alert('Invalid category');
+      }
+    } else {
+      alert('Invalid credentials');
+    }
+  };
+
+  const handleCategoryChange = (e) => {
+    setSelectedCategory(e.target.value);
+  };
+
   return (
-    <div>Login</div>
-  )
-}
+    <div className="login-box">
+      <form>
+      <h3 className="welcome">Enter your credentials to login!</h3>
+        <div className='in-box'>
+          <label className="email">Email:</label>
+          <input
+            className="box"
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="pass-box">
+          <label className="email">Password:</label>
+          <input
+            className='p-box'
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="category">
+          <label>Category:</label>
+          <div className="toggle-container">
+            <label>
+              <input
+                type="radio"
+                name="category"
+                value="representative"
+                checked={selectedCategory === 'representative'}
+                onChange={handleCategoryChange}
+              />
+              Representative
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="category"
+                value="team-lead"
+                checked={selectedCategory === 'team-lead'}
+                onChange={handleCategoryChange}
+              />
+              Team Lead
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="category"
+                value="operations"
+                checked={selectedCategory === 'operations'}
+                onChange={handleCategoryChange}
+              />
+              Operations
+            </label>
+          </div>
+        </div>
+        <button className="loginbutton" type="button" onClick={handleLogin}>
+          Login
+        </button>
+        <p className="reg-text">
+          Don't have an account? <Link to="/register">Register</Link>
+        </p>
+      </form>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
