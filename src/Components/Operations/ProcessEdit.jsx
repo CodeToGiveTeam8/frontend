b70fe5321f2b23280e22d6b699cc4a56688dc002
Surya Prasath <suryaprasath.5207@gmail.com>
2023-06-07 12:22:21 +0530

    connected home and operation page with backend

--- src/Components/Operations/ProcessEdit.jsx
+++ src/Components/Operations/ProcessEdit.jsx
@@ -1,32 +1,50 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import '../CSSstyles/ProcessEdit.css';
+import Cookies from 'universal-cookie';
+
 
 function ProcessEdit() {
   // Assuming you have an array of data containing S.No., Step, and Sub-steps
-  const initialData = [
-    { sNo: 1, step: 'Work on and complete documentation', subSteps: [{ subStep: 'Newspaper Publication', completed: false }, { subStep: 'TV Telecasting', completed: false }, { subStep: 'File Missing Compliant, if not already done', completed: false }] },
-    { sNo: 2, step: 'Submit to DCPU and get NOC', subSteps: [{ subStep: 'Submit child’s report for DCPU for NOC', completed: false }, { subStep: 'Receive DCPU NOC', completed: false }] },
-    { sNo: 3, step: 'Work on and complete documentationWork on and complete documentationWork on and complete documentationWork on and complete documentationWork on and complete documentation', subSteps: [{ subStep: 'Sub-step 3.1', completed: false }, { subStep: 'Sub-step 3.2', completed: false }, { subStep: 'Sub-step 3.3', completed: false }, { subStep: 'Sub-step 3.4', completed: false }] },
-    // Add more data as needed
-  ];
+  const initialData = [];
+  const cookies = new Cookies();  //needed to parse cookies
 
   const [data, setData] = useState(initialData);
   const [showPopup, setShowPopup] = useState(false);
 
-  const handleCheckboxChange = (stepIndex, subStepIndex) => {
-    const updatedData = [...data];
-    const checked = !updatedData[stepIndex].subSteps[subStepIndex].completed;
-    updatedData[stepIndex].subSteps[subStepIndex].completed = checked;
-    setData(updatedData);
-
-    if (checked) {
-      setShowPopup(true); // Show the pop-up only when the checkbox is checked
-    }
-  };
-
   const closePopup = () => {
     setShowPopup(false); // Close the pop-up
   };
+
+  const getProcessData = (configObject)=>{
+    return new Promise((resolve,reject)=>{
+      fetch(configObject.url,{
+        method:configObject.method?configObject.method:'GET',
+        body:configObject.body?JSON.stringify(configObject.body):null,
+        headers:configObject.headers?configObject.headers:{},
+      }).then((response)=>resolve(response.json()))
+    })
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+
+        const token = cookies.get("accessToken"); // to get token already present.If there is token ,login page should not be rendered
+        const configObject = {
+          url:"http://localhost:8081/process/?category=ABANDONED",
+          method:'GET',
+          headers:{'Content-Type':'application/json','Authorization': token},
+        }
+        const responseData = await getProcessData(configObject)
+        const data = responseData['data']
+        setData(data)
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div className="table-container">
@@ -42,26 +60,14 @@ function ProcessEdit() {
           {data.map((item, stepIndex) => (
             <React.Fragment key={stepIndex}>
               <tr>
-                <td rowSpan={item.subSteps.length}>{item.sNo}</td>
-                <td rowSpan={item.subSteps.length}>{item.step}</td>
-                <td>
-                  <input
-                    type="checkbox"
-                    checked={item.subSteps[0].completed || false}
-                    onChange={() => handleCheckboxChange(stepIndex, 0)}
-                  />
-                  {item.subSteps[0].subStep}
-                </td>
+                <td rowSpan={item.subProcess.length + 1}>{item.orderNo}</td>
+                <td rowSpan={item.subProcess.length + 1}>{item.process[0].name}</td>
               </tr>
-              {item.subSteps.slice(1).map((subStep, subStepIndex) => (
+              {item.subProcess.map((subStep, subStepIndex) => (
                 <tr key={subStepIndex}>
                   <td>
-                    <input
-                      type="checkbox"
-                      checked={subStep.completed || false}
-                      onChange={() => handleCheckboxChange(stepIndex, subStepIndex + 1)}
-                    />
-                    {subStep.subStep}
+                    <input disabled type="checkbox"/>
+                    {subStep.name}
                   </td>
                 </tr>
               ))}
